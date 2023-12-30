221da368773d5be1021e1c6877fc42dde3ffb33c
Jannik Köster <jannik.koester@myleodsc.com>
2023-12-30 18:39:29 +0100

    added sveltekit

--- bun/bun-hono-drizzle/drizzle.config.ts
+++ bun/bun-hono-drizzle/drizzle.config.ts
@@ -1,5 +1,9 @@
 import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/db/schema.ts",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL!,
+  },
   out: "./drizzle",
 } satisfies Config;
--- bun/bun-hono-drizzle/src/db/migration/index.ts
+++ bun/bun-hono-drizzle/src/db/migration/index.ts
@@ -3,6 +3,7 @@ import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
 const connectionString = process.env.DATABASE_URL!;
+console.log("Migrating database:", connectionString);
 const sql = postgres(connectionString, { max: 1 });
 const db = drizzle(sql);
 
